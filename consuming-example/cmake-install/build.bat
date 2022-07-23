919e48c886085f529737b0bc74ff82d72b09b055
Damien Pasqualinotto <93010861+GroPask@users.noreply.github.com>
2022-07-23 13:57:25 +0200

    Add consuming example with CMakeInstall

---
+++ consuming-example/cmake-install/build.bat
@@ -0,0 +1,13 @@
+set libDir=%~dp0/third-party/
+set installDir=%libDir%/install
+
+git clone https://github.com/GroPask/ImApp.git %libDir%/ImAppSrc
+
+cmake -G "Visual Studio 16 2019" -DCMAKE_INSTALL_PREFIX=%installDir% -DIMAPP_BUILD_TESTS=OFF -S %libDir%/ImAppSrc -B %libDir%/ImAppBuild
+cmake --build %libDir%/ImAppBuild --config Release
+cmake --install %libDir%/ImAppBuild --config Release
+
+cmake -G "Visual Studio 16 2019" -DCMAKE_PREFIX_PATH=%installDir% -S %~dp0 -B %~dp0/build
+cmake --build %~dp0/build --config Release
+
+pause
