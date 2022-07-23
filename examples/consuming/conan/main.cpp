4cb3db3b71e806141d8fddd95a988213699a3adb
Damien Pasqualinotto <93010861+GroPask@users.noreply.github.com>
2022-07-23 17:40:53 +0200

    Add Conan example

---
+++ examples/consuming/conan/main.cpp
@@ -0,0 +1,14 @@
+#include <ImApp/ImApp.hpp>
+
+int main(int, char**)
+{
+    ImApp::Application app(1280, 720, "ImApp_ConsumingExample_Conan");
+
+    return app.run([&]()
+    {
+        ImGui::Text("Hello World");
+
+        if (ImGui::Button("Close"))
+            app.close();
+    });
+}
