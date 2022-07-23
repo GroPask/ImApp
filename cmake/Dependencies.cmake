ab6a91a938f3c783dab38f1748611391cbba7ac2
Damien Pasqualinotto <93010861+GroPask@users.noreply.github.com>
2022-07-23 15:22:32 +0200

    Refacto ImGui CMake files generation

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -24,51 +24,22 @@ endif ()
 
 find_package(imgui QUIET)
 if (NOT imgui_FOUND)
+    set(IMGUI_INSTALL ${IMAPP_INSTALL} CACHE BOOL "" FORCE)
+
     FetchContent_Declare(imgui
         GIT_REPOSITORY https://github.com/ocornut/imgui.git
         GIT_TAG 9cd9c2eff99877a3f10a7f9c2a3a5b9c15ea36c6
     )
+
     FetchContent_GetProperties(imgui)
     if(NOT imgui_POPULATED)
         FetchContent_Populate(imgui)
 
         file(COPY ${imgui_SOURCE_DIR}/backends/ DESTINATION ${CMAKE_BINARY_DIR}/ImGuiBackendsImpl)
 
-        file(DOWNLOAD
-            https://raw.githubusercontent.com/conan-io/conan-center-index/5a7dc52e8ae19eead370591a6d2fa5e4953f0e68/recipes/imgui/all/CMakeLists.txt
-            ${imgui_SOURCE_DIR}/CMakeLists.txt
-        )
+        include(ImGuiCMakeGenerator.cmake)
+        generateImGuiCMakeFiles(${imgui_SOURCE_DIR})
 
-        file(READ ${imgui_SOURCE_DIR}/CMakeLists.txt imGuiCMakeListsContent)
-        string(REPLACE "project(imgui" "project(imgui VERSION 1.88 LANGUAGES" imGuiCMakeListsContent "${imGuiCMakeListsContent}")
-        string(REPLACE "include(conanbuildinfo.cmake)" "" imGuiCMakeListsContent "${imGuiCMakeListsContent}")
-        string(REPLACE "conan_basic_setup(TARGETS)" "" imGuiCMakeListsContent "${imGuiCMakeListsContent}")
-        string(REPLACE "/source_subfolder" "" imGuiCMakeListsContent "${imGuiCMakeListsContent}")
-        string(REPLACE [=[add_executable(${BINARY_TO_COMPRESSED_BIN} ${EXTRA_FONTS_DIR}/binary_to_compressed_c.cpp)]=] "" imGuiCMakeListsContent "${imGuiCMakeListsContent}")
-        string(REPLACE [=[add_library(${PROJECT_NAME} ${SOURCE_FILES})]=] [=[add_library(${PROJECT_NAME} ${SOURCE_FILES} ${HEADER_FILES})
-add_library(${PROJECT_NAME}::${PROJECT_NAME} ALIAS ${PROJECT_NAME})]=] imGuiCMakeListsContent "${imGuiCMakeListsContent}")
-        string(REPLACE "include(GNUInstallDirs)" [=[option(IMGUI_INSTALL "Generate the install target" ON)
-if (IMGUI_INSTALL)
-include(GNUInstallDirs)]=] imGuiCMakeListsContent "${imGuiCMakeListsContent}")
-        string(REPLACE [=[install(TARGETS ${PROJECT_NAME}]=] [=[install(TARGETS ${PROJECT_NAME} EXPORT ${PROJECT_NAME}Targets]=] imGuiCMakeListsContent "${imGuiCMakeListsContent}")
-        string(REPLACE [=[install(TARGETS ${BINARY_TO_COMPRESSED_BIN}
-        DESTINATION ${CMAKE_INSTALL_BINDIR})]=] "" imGuiCMakeListsContent "${imGuiCMakeListsContent}")
-        file(WRITE ${imgui_SOURCE_DIR}/CMakeLists.txt "${imGuiCMakeListsContent}")
-
-        file(APPEND ${imgui_SOURCE_DIR}/CMakeLists.txt [=[include(CMakePackageConfigHelpers)
-configure_package_config_file(ImGuiConfig.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/ImGuiConfig.cmake INSTALL_DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME})
-write_basic_package_version_file(${CMAKE_CURRENT_BINARY_DIR}/ImGuiConfigVersion.cmake COMPATIBILITY SameMajorVersion)
-install(EXPORT ${PROJECT_NAME}Targets DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME} NAMESPACE ${PROJECT_NAME}:: FILE ${PROJECT_NAME}Targets.cmake)
-install(FILES ${CMAKE_CURRENT_BINARY_DIR}/ImGuiConfig.cmake ${CMAKE_CURRENT_BINARY_DIR}/ImGuiConfigVersion.cmake DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME})
-endif (IMGUI_INSTALL)
-]=])
-
-        file(WRITE ${imgui_SOURCE_DIR}/ImGuiConfig.cmake.in [=[@PACKAGE_INIT@
-include ( "${CMAKE_CURRENT_LIST_DIR}/ImGuiTargets.cmake" )
-check_required_components(ImGui)
-]=])
-
-        set(IMGUI_INSTALL ${IMAPP_INSTALL} CACHE BOOL "" FORCE)
         add_subdirectory(${imgui_SOURCE_DIR} ${imgui_BINARY_DIR})
     endif ()
 endif ()
