cff08e8e811770ff1b3611ad8f543af54e79a80d
Damien Pasqualinotto <93010861+GroPask@users.noreply.github.com>
2022-07-23 07:41:20 +0200

    Download dependencies if needed

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -7,3 +7,49 @@ FetchContent_Declare(dpCMake
 FetchContent_MakeAvailable(dpCMake)
 
 include(${dpcmake_SOURCE_DIR}/dpCMake.cmake)
+
+find_package(glfw3 QUIET)
+if (NOT glfw3_FOUND)
+    set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+    set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+    set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
+    set(GLFW_INSTALL ON CACHE BOOL "" FORCE)
+
+    FetchContent_Declare(glfw3
+        GIT_REPOSITORY https://github.com/glfw/glfw.git
+        GIT_TAG 3.3.6
+    )
+    FetchContent_MakeAvailable(glfw3)
+
+    find_package(OpenGL REQUIRED)
+
+    target_link_libraries(glfw PUBLIC OpenGL::GL)
+endif ()
+
+find_package(imgui QUIET)
+if (NOT imgui)
+    FetchContent_Declare(imgui
+        GIT_REPOSITORY https://github.com/ocornut/imgui.git
+        GIT_TAG 9cd9c2eff99877a3f10a7f9c2a3a5b9c15ea36c6
+    )
+    FetchContent_GetProperties(imgui)
+    if(NOT imgui_POPULATED)
+        FetchContent_Populate(imgui)
+
+        file(COPY ${imgui_SOURCE_DIR}/backends/ DESTINATION ${CMAKE_BINARY_DIR}/ImGuiBackendsImpl)
+
+        file(DOWNLOAD
+            https://raw.githubusercontent.com/conan-io/conan-center-index/5a7dc52e8ae19eead370591a6d2fa5e4953f0e68/recipes/imgui/all/CMakeLists.txt
+            ${imgui_SOURCE_DIR}/CMakeLists.txt
+        )
+        file(READ ${imgui_SOURCE_DIR}/CMakeLists.txt imGuiCMakeListsContent)
+        string(REPLACE "include(conanbuildinfo.cmake)" "" imGuiCMakeListsContent "${imGuiCMakeListsContent}")
+        string(REPLACE "conan_basic_setup(TARGETS)" "" imGuiCMakeListsContent "${imGuiCMakeListsContent}")
+        string(REPLACE "/source_subfolder" "" imGuiCMakeListsContent "${imGuiCMakeListsContent}")
+        string(REPLACE [=[add_library(${PROJECT_NAME} ${SOURCE_FILES})]=] [=[add_library(${PROJECT_NAME} ${SOURCE_FILES} ${HEADER_FILES})
+add_library(${PROJECT_NAME}::${PROJECT_NAME} ALIAS ${PROJECT_NAME})]=] imGuiCMakeListsContent "${imGuiCMakeListsContent}")
+        file(WRITE ${imgui_SOURCE_DIR}/CMakeLists.txt "${imGuiCMakeListsContent}")
+
+        add_subdirectory(${imgui_SOURCE_DIR} ${imgui_BINARY_DIR})
+    endif ()
+endif ()
