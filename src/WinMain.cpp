9ad8d6556edbf0f09d1065b4c4b3e9121f41f889
Damien Pasqualinotto <93010861+GroPask@users.noreply.github.com>
2022-07-23 18:55:20 +0200

    Add WinMain on Windows

---
+++ src/WinMain.cpp
@@ -0,0 +1,8 @@
+#include <Windows.h>
+
+int main(int argc, char** argv);
+
+int WinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, PSTR /*lpCmdLine*/, INT /*nCmdShow*/)
+{
+    return main(__argc, __argv);
+}
